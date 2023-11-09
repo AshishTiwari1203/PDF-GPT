ea863ba039c6e186e2e830ebe07508fd56ffd77f
Ashish <ashishktiwari1203@gmail.com>
2023-11-09 23:38:37 +0530

    Made the GUI

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-#Loading .ev
-
+import streamlit as st
 from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title = "PDF's GPT" , page_icon=":books:")
+    st.header("Chat With Your PDF's :books:")
+    st.text_input("Ask Me Anything..")
+
+    with st.sidebar:
+        st.subheader("It reads your Documents")
+        st.file_uploader("Import Your PDF's here")
+        st.button("Submit")
+
+
+if __name__ == '__main__':
+    main()
